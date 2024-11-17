68eaae70eb5fa4fa83e6ad78c786dd859f2ded79
shpark <shp04078@naver.com>
2024-11-17 23:32:43 +0900

    Add files via upload

---
+++ board_back/controller/BoardController.java
@@ -0,0 +1,19 @@
+package board.board_fullstack.board_back.controller;
+
+import board.board_fullstack.board_back.model.vo.Board;
+
+import java.util.Date;
+
+public class BoardController {
+    private Board board = new Board();
+
+    public Board boardPost(int no, int view, String title, String detail, String userId, Date time){
+        board.setNo(no);
+        board.setView(view);
+        board.setTitle(title);
+        board.setDetail(detail);
+        board.setUserId(userId);
+        board.setTime(time);
+        return board;
+    }
+}
---
+++ board_back/controller/UserController.java
@@ -0,0 +1,46 @@
+package board.board_fullstack.board_back.controller;
+
+import board.board_fullstack.board_back.model.vo.User;
+
+import javax.sql.*;
+import java.sql.*;
+import java.sql.Date;
+
+import static jdbc.db.stmt;
+
+public class UserController {
+    private final DataSource dataSource;
+    private User user = new User();
+    private ResultSet rs = null;
+
+    public UserController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public User userPost(String name, String id, String pwd, String phone, String email, Date date, String address) throws SQLException {
+        try {
+
+            rs = stmt.executeQuery("");
+            if(rs.next()){
+                user.setName(rs.getString(1));
+                user.setId(rs.getString(2));
+                user.setPhone(rs.getString(3));
+                user.setPwd(rs.getString(4));
+                user.setEmail(rs.getString(5));
+                user.setDate(rs.getDate(6));
+                user.setAddress(rs.getString(7));
+            }else{
+                throw new SQLException("회원가입 실패");
+            }
+            return user;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+
+        }
+    }
+
+    public boolean userPut(String name, String id, String phone, String pwd, String email, Date date,String address){
+        return false;
+    }
+}
